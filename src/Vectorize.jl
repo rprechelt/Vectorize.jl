0022bf0e0384a0c12d48d569ad3a0ebcaced6e24
Remy Prechelt <rprechelt@gmail.com>
2016-05-22 10:11:32 -0500

    Fully functioning CMake process for building pass out of source

--- src/Vectorize.jl
+++ src/Vectorize.jl
@@ -1,4 +1,24 @@
 module Vectorize
 
+export @vectorize
+
+# macro vectorize(ex)
+#     esc(vectorize_(ex))
+# end
+
+macro vectorize(ex)
+    dump(ex)
+    isa(ex, Expr) || error("@vectorize expects an expression as its only argument")
+
+    ## Currently only support function calls
+    ex.head == :call || error("@vectorize currently only supports function calls")
+
+    ex
+end
+
+function vrize()
+	 :($(Expr(:vectorize)))
+end	
+
 
 end # module
