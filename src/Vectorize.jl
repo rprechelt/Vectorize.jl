6a8ed6e9028bd1f46714e37fedd8bd8bfd563232
Remy Prechelt <rprechelt@gmail.com>
2016-05-22 22:15:05 -0500

    Updated macro definition

--- src/Vectorize.jl
+++ src/Vectorize.jl
@@ -2,23 +2,8 @@ module Vectorize
 
 export @vectorize
 
-# macro vectorize(ex)
-#     esc(vectorize_(ex))
-# end
-
 macro vectorize(ex)
-    dump(ex)
-    isa(ex, Expr) || error("@vectorize expects an expression as its only argument")
-
-    ## Currently only support function calls
-    ex.head == :call || error("@vectorize currently only supports function calls")
-
-    ex
+    esc(isa(ex, Expr) ? pushmeta!(ex, :vectorize) : ex)
 end
-
-function vrize()
-	 :($(Expr(:vectorize)))
-end	
-
 
 end # module
