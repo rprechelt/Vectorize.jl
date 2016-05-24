0e2211fc4aba4b2218d585ce4c8e6483202ad2c7
Remy Prechelt <rprechelt@gmail.com>
2016-05-23 20:47:44 -0500

    Updated Accelerate mappings

--- src/Accelerate.jl
+++ src/Accelerate.jl
@@ -2,10 +2,10 @@ module Accelerate
 
 const libacc = "/System/Library/Frameworks/Accelerate.framework/Accelerate"
 
-## This defines the mapping between Julia function names and Accelerate names. Each tuple is of the form
+## This defines the mapping between Julia function names and Accelerate vecLib names. Each tuple is of the form
 ## (:julia_name, :accelerate_name). The accelerate names are stored without type suffix; it is assumed that
-## there is no suffix for Float32, and "D" suffix for Float64. 
-const AccelerateFunctions =
+## there is no suffix for Float64, and "f" suffix for Float32. 
+const vecLibFunctions =
     ((:ceil, :vvceil),
      (:floor, :vvfloor),
      (:sqrt, :vvsqrt),
@@ -26,7 +26,7 @@ const AccelerateFunctions =
      (:tanpi, :vvtanpi),
      (:asin, :vvasin),
      (:acos, :vvacos),
-     (:atan, :vvatan), 
+     (:atan, :vvatan),
      (:sinh, :vvsinh),
      (:cosh, :vvcosh),
      (:tanh, :vvtanh),
@@ -34,23 +34,24 @@ const AccelerateFunctions =
      (:acosh, :vvacosh),
      (:atanh, :vvatanh))
 
+## For vecLibFunctions
 for (T, suff) in ((Float64, ""), (Float32, "f"))
 
-    for (f, fa) in AccelerateFunctions
+    for (f, fa) in vecLibFunctions
         f! = symbol("$(f)!")
         @eval begin
             function ($f)(X::Vector{$T})
                 out = similar(X)
-                ($f!)(out, X)
+                return ($f!)(out, X)
             end
             function ($f!)(out::Vector{$T}, X::Vector{$T})
                 ccall(($(string(f,suff)),libacc),Void,
-                      (Ptr{$T},Ptr{$T},Ptr{Cint}),out,X,&length(X))
-                out
+                      (Ptr{$T},Ptr{$T},Ptr{Cint}),
+                      out,X,&length(X))
+                return out
             end
         end
     end
-
 end
 
 end # module
