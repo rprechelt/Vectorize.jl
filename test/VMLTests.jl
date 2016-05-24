f76cc41d294f04d0658bfeffe8ef97566e718a46
Remy Prechelt <rprechelt@gmail.com>
2016-05-23 23:57:01 -0500

    Added empty testing files

---
+++ test/VMLTests.jl
@@ -0,0 +1,7 @@
+using Vectorize
+if VERSION >= v"0.5-"
+    using Base.Test
+else
+    using BaseTestNext
+    const Test = BaseTestNext
+end
--- test/runtests.jl
+++ test/runtests.jl
@@ -5,3 +5,17 @@ else
     using BaseTestNext
     const Test = BaseTestNext
 end
+
+## Test Apple Accelerate
+@osx? include("AccelerateTests.jl") : println("Accelerate not present on current system. Aborting Accelerate tests")
+
+## TODO: Check for presence of Yeppp!
+include("YepppTests.jl")
+
+## TODO: Check for presence of VML
+include("VMLTests.jl")
+
+
+#################### VECTORIZE TESTS ####################
+
+
