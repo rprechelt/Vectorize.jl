93450b5fc6fa80bca2544aa0c0ffc335c72d2372
Remy Prechelt <rprechelt@gmail.com>
2016-06-06 22:53:07 -0500

    Added initial Yeppp library to Vectorize

--- src/Yeppp.jl
+++ src/Yeppp.jl
@@ -1,4 +1,16 @@
-__precompile__()
-baremodule Yeppp!
+#__precompile__()
+module Yeppp
+
+## Find Yeppp library
+currdir = @__FILE__
+bindir = currdir[1:end-4]*"deps/src/yeppp/binaries/"
+if OS_NAME == :Darwin
+    @eval const libyeppp = bindir*"macosx/x86_64/libyeppp.dylib"
+elseif OS_NAME == :Linux
+    @eval const libyeppp = bindir*"linux/x86_64/libyeppp.so"
+elseif OS_NAME == :Windows
+    @eval const libyeppp = bindir*"windows/amd64/yeppp.dll"
+end
+
 
 end # End Module
