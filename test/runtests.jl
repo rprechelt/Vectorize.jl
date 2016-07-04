e6d9454e3db87fafac286a4b8cabfcc0ffbad0ba
Remy Prechelt <rprechelt@gmail.com>
2016-07-04 01:11:59 -0500

    Added support for coveralls.io

--- test/runtests.jl
+++ test/runtests.jl
@@ -11,15 +11,17 @@ println("===== Testing Accelerate =====")
 @osx? include("AccelerateTests.jl") : println("Accelerate not present on current system. Aborting Accelerate tests")
 println("===== Accelerate Tests Successful =====\n\n")
 
-## TODO: Check for presence of Yeppp!
+## Check for presence of Yeppp!
 println("===== Testing Yeppp!  =====")
 include("YepppTests.jl")
 println("===== Yeppp! Tests Successful =====\n\n")
 
-## TODO: Check for presence of VML
-println("===== Testing Intel's VML =====")
-include("VMLTests.jl")
-println("===== VML Tests Successful =====\n\n")
+## Check for presence of VML
+if Libdl.find_library(["libmkl_vml_avx"], ["/opt/intel/mkl/lib"]) != ""
+    println("===== Testing Intel's VML =====")
+    include("VMLTests.jl")
+    println("===== VML Tests Successful =====\n\n")
+end
 
 
 #################### VECTORIZE TESTS ####################
