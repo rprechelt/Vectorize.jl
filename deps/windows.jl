505eee852c8fe577de8e224451ed0cad9c22d422
Remy Prechelt <rprechelt@gmail.com>
2016-08-16 23:42:32 -0500

    Removed win32 from appveyor

--- deps/windows.jl
+++ deps/windows.jl
@@ -7,7 +7,7 @@ else
     trycmd(`mkdir $(pkgdir)deps\downloads`)
     trycmd(`mkdir $(pkgdir)deps\src`)
     trycmd(`mkdir $(pkgdir)deps\src\yeppp`)
-    trycmd(`wget -OutFile downloads 'http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-1.0.0.tar.bz2'`, err="Unable to download Yeppp!")
+    trycmd(`wget 'http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-1.0.0.tar.bz2' -OutFile "downloads"`, err="Unable to download Yeppp!")
     trycmd(`tar -xjvf $(pkgdir)deps\downloads\yeppp-1.0.0.tar.bz2 -C $(pkgdir)deps\src\yeppp --strip-components=1`)
     info("====== Successfully installed Yeppp! ======")
 end
