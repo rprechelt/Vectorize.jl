30e1e1ea42ed6f97fa5dc90b28446339dfc720e2
Remy Prechelt <rprechelt@gmail.com>
2016-08-16 23:36:40 -0500

    Updated windows wget syntax

--- deps/windows.jl
+++ deps/windows.jl
@@ -7,8 +7,7 @@ else
     trycmd(`mkdir $(pkgdir)deps\downloads`)
     trycmd(`mkdir $(pkgdir)deps\src`)
     trycmd(`mkdir $(pkgdir)deps\src\yeppp`)
-    yeppploc = "http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-1.0.0.tar.bz2"
-    trycmd(`wget -P downloads $(yeppploc)`, err="Unable to download Yeppp!")
+    trycmd(`wget -OutFile downloads 'http://bitbucket.org/MDukhan/yeppp/downloads/yeppp-1.0.0.tar.bz2'`, err="Unable to download Yeppp!")
     trycmd(`tar -xjvf $(pkgdir)deps\downloads\yeppp-1.0.0.tar.bz2 -C $(pkgdir)deps\src\yeppp --strip-components=1`)
     info("====== Successfully installed Yeppp! ======")
 end
