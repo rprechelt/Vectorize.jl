b24799b845e617a375b52ed782de9b68830cda71
Remy Prechelt <rprechelt@gmail.com>
2016-08-17 10:55:57 -0500

    Updates to cleaning process for windows

--- deps/clean.jl
+++ deps/clean.jl
@@ -13,5 +13,11 @@ currdir = @__FILE__
 pkgdir = currdir[1:end-13]
 
 # Clean up directory status
-run(`rm -rf $(pkgdir)src/Functions.jl`)
-run(`touch $(pkgdir)src/Functions.jl`)
+@static if is_windows()
+    run(`del $(pkgdir)src\Functions.jl`)
+    run(`copy NUL Functions.jl`)
+    run(`move Functions.jl $(pkdir)src`)
+else
+    run(`rm -rf $(pkgdir)src/Functions.jl`)
+    run(`touch $(pkgdir)src/Functions.jl`)
+end
